0527d24cdc818b57e77289d7463b91e7693ff908
elmyrza <ekudaibergenov16@gmail.com>
2023-09-16 15:14:43 +0600

    feat: implement int-reverse task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+  let str = String(n).split("").reverse()
+  if (str[str.length - 1] === "-") str = str.splice(0, str.length - 1)
+  return Number(str.join(""))
 }
